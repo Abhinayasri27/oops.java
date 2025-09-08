4268071509df6dffa85b3dac8c9e97f9f018655f
Abhinayasri27 <25wh5a0535@bvrithyderabad.edu.in>
2025-09-08 15:55:22 +0530

    Create operations.java

---
+++ operations.java
@@ -0,0 +1,18 @@
+public class OperationsExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+
+        System.out.println("a + b = " + sum);
+        System.out.println("a - b = " + difference);
+        System.out.println("a * b = " + product);
+        System.out.println("a / b = " + quotient);
+        System.out.println("a % b = " + remainder);
+    }
+}
